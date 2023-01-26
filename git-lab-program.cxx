ce5cfa7905a1dd9a3a22fefaa958b433aac9d6c8
Will Muskopf <wm622521@ohio.edu>
2023-01-25 19:05:58 -0500

    commiting hello git program

---
+++ git-lab-program.cxx
@@ -0,0 +1,19 @@
+/**
+ *        @file: git-lab-program.cxx
+ *      @author: Will Muskopf
+ *        @date: 2023.01.25
+ * @description: Prints "Hello, Git!" to the screen
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>  //include libraries
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+    cout << "Hello, Git!" << endl;
+
+    return 0;
+}// main
